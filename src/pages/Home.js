ecf72071f557dc4c3f466fcf2dbfabb33807d9e0
Erick Palomino <erickp1502@gmail.com>
2024-12-14 00:43:38 -0500

    feat: simulate date for demo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Paper } from '@mui/material';
+import { Button, Grid, Input, Paper, TextField } from '@mui/material';
 import * as React from 'react';
 import { Chart } from "react-google-charts";
 import { CovAxios } from '../utils/AxiosClient';
@@ -6,8 +6,9 @@ import { useEffect,useState } from 'react';
 
 export default function HomePage() {
     const [chartData, setchartData] = useState([]);
+    const [selectedDate, setSelectedDate] = useState('');
     useEffect(() => {
-        CovAxios.get("/infection/7days").
+        CovAxios.get("/infection/7days?date=2022-02-13").
             then((response) => {
                 console.log(response.data)
                 console.log(reshapeChartData(response.data))
@@ -17,14 +18,30 @@ export default function HomePage() {
     
 
     const handlePredictButton = ()=>{
-        CovAxios.get("/infection/7days/predict").
+        CovAxios.get("/infection/7days/predict?date="+selectedDate).
             then((response) => {
                 console.log(response.data)
                 console.log(reshapeChartData(response.data))
                 setchartData(reshapeChartData(response.data))
             });
     }
+    const handleDateChange = (event) => { 
+        console.log(event.target.value)
+        setSelectedDate(event.target.value); 
+        CovAxios.get("/infection/7days?date="+selectedDate).
+            then((response) => {
+                console.log(response.data)
+                console.log(reshapeChartData(response.data))
+                setchartData(reshapeChartData(response.data))
+            });
     
+    };
+
+    const handleCheckDateButton = ()=>{
+        console.log(selectedDate)
+        
+    }
+
     return (
         <Grid
             container
@@ -34,12 +51,15 @@ export default function HomePage() {
             justifyContent="center"
             style={{ minHeight: '100vh' }}
         >
+            <TextField defaultValue="2022-02-13" type="date" id="date" name="date" value={selectedDate} onChange={handleDateChange} variant="outlined" label="Simular fecha" InputLabelProps={{ shrink: true, }} />
             <Grid item xs={12} p={5} >
                 <Chart
                     chartType="ColumnChart"
                     data={chartData}
                     options={
-                        {title: 'Infección de COVID 19 en el Perú'}
+                        {title: 'Infección de COVID 19 en el Perú',
+                            vAxis: { minValue: 0 }
+                        }
                     }
                     width={"800px"}
                     height={"600px"}
