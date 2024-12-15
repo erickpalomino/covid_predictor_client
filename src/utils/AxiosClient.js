9c7ffeb0753e1bc0cd7cfbba808ca4c4c6f537bc
Erick Palomino <erickp1502@gmail.com>
2024-12-15 16:34:43 -0500

    fix: ngrok skip warning

--- src/utils/AxiosClient.js
+++ src/utils/AxiosClient.js
@@ -5,6 +5,7 @@ export const CovAxios = axios.create({
     baseURL: process.env.REACT_APP_BACK_URL,
     headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true'
       }
   });
